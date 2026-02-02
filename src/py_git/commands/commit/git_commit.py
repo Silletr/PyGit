5cb386da9c411953a3736bb61a3ad25a9dbd8c5e
Silletr <hristicenkoigor764@gmail.com>
2026-02-02 22:21:52 +0200

    !!! JUST A TEST OF THIS SCRIPT !!!

--- src/py_git/commands/commit/git_commit.py
+++ src/py_git/commands/commit/git_commit.py
@@ -1,9 +1,10 @@
 # --- IMPORTS ---
+from subprocess import run, CalledProcessError
 from loguru import logger
 from shutil import which
 
 # --- GIT PATH ---
-git = which("git")
+git = str(which("git"))
 
 # --- LOGGER CONFIG ---
 logger.add(
@@ -15,11 +16,23 @@ logger.add(
 
 
 # --- COMMIT FUNCTION ---
-def git_commit(
-    commit_message: str = "",
-    changed_files: str = "",
-):
-    logger.info(f"Commit message: {commit_message} | Changed files: {changed_files}")
+def git_commit(commit_message: str = "", changed_files: str = ""):
+    try:
+        # Stage files first (handle single file or space-separated list)
+        files_list = changed_files.split() if changed_files else [""]
+        if files_list[0]:  # If files provided
+            run([git, "add"] + files_list, check=True, capture_output=True)
+            logger.info(f"Staged files: {changed_files}")
+
+        # Commit
+        result = run(
+            [git, "commit", "-m", commit_message], check=True, capture_output=True
+        )
+        logger.info(f"Commit successful: {commit_message} | Files: {changed_files}")
+        logger.info(f"Output: {result}")
+    except CalledProcessError as e:
+        logger.error(f"Git error: {e.stderr.decode()}")
+        raise
 
 
 changed_file = input("Changed files: ")
