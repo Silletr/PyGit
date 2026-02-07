3a5a59ea6d568652683b95f4f1a30ab1979132a7
Silletr <hristicenkoigor764@gmail.com>
2026-02-07 18:14:06 +0200

    [CHANGED FILE/DIR: src/py_git/commands/clone/gh_clone.py] A little bit refactor for better readability [TIMESTAMP: 07.02.2026 | 18:13]

--- src/py_git/commands/clone/gh_clone.py
+++ src/py_git/commands/clone/gh_clone.py
@@ -7,7 +7,8 @@ from subprocess import run, CalledProcessError
 # --- gh PATH ---
 gh_path = which("gh")
 if not gh_path:
-    logger.critical("GITHUB CLI (gh) NOT FOUND. Install it: sudo apt install gh")
+    logger.critical(
+        "GITHUB CLI (gh) NOT FOUND. Install it: sudo apt install gh")
     exit(1)
 gh = str(gh_path)
 
@@ -25,13 +26,15 @@ def clone_repo(repo_owner: str = "", repository_name: str = ""):
     logger.info(f"Executing: gh repo clone {repo_url}")
 
     try:
-        run([gh, "repo", "clone", repo_url], capture_output=True, text=True, check=True)
+        run([gh, "repo", "clone", repo_url],
+            capture_output=True, text=True, check=True)
 
         logger.success(f"Successfully cloned {repo_url} to {repository_name}/")
         return 0
 
     except CalledProcessError as e:
-        logger.error(f"Clone failed: {e}\nSTDOUT: {e.stdout}\nSTDERR: {e.stderr}")
+        logger.error(f"Clone failed: {e}\nSTDOUT: {
+                     e.stdout}\nSTDERR: {e.stderr}")
         return 1
     except FileNotFoundError:
         logger.error("gh command not found in PATH")
